97a9f49e5d0a009e6e5070776112197e1c19a7e1
sendy <sendy@duakelinci.com>
2025-01-28 15:00:24 +0700

    update sidebar icons and add new Master Data section

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -8,14 +8,14 @@
     <ul class="sidebar-menu">
         @section('sidebar')
             <li class="menu-header">Dashboard</li>
-            <li class=""><a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+            <li class=""><a href="#" class="nav-link"><i class="fas fa-signal"></i><span>Dashboard</span></a></li>
 
             <li class="menu-header">Settings</li>
-
+            
             <li class=""><a href="#" class="nav-link"><i class="fas fa-users"></i><span>User Management</span></a></li>
-
+            
             <li class="nav-item dropdown">
-              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Access Control</span></a>
+              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-universal-access"></i> <span>Access Control</span></a>
               <ul class="dropdown-menu">
                 <li><a class="nav-link" href="#">User Roles</a></li>
                 <li><a class="nav-link" href="#">Roles Permissions</a></li>
@@ -23,12 +23,18 @@
               </ul>
             </li>
 
+            <li class="menu-header">Master Data</li>
+
+            <li class=""><a href="#" class="nav-link"><i class="fas fa-calendar-week"></i><span>Master Day</span></a></li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-dice-d6"></i> <span>Apps Control</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="#">Server Apps</a></li>
+                <li><a class="nav-link" href="#">Apps Modules</a></li>
+                <li><a class="nav-link" href="#">Modules Menu</a></li>
+              </ul>
+            </li>
         @show
       </ul>
-
-      {{-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-        <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-          <i class="fas fa-rocket"></i> Documentation
-        </a>
-      </div> --}}
   </aside>
